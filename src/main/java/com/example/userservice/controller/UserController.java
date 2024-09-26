37740e9d7868dfe072dd3f059cf03f612fab6740
pavan147 <31723029+pavan147@users.noreply.github.com>
2024-09-26 15:03:10 +0530

    updated the return message

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/status-check")
     public String statusCheck(){
-        return "Working";
+        return "Working User Service.........";
     }
 }
 
